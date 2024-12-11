7103f9ee3e58c85d857953f43815f2acabe6c691
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-12-11 19:45:10 +0000

    fix: remove public abstract from interface methods

--- Java/AlertService.java
+++ Java/AlertService.java
@@ -3,37 +3,39 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+/* all methods in an interface are inherently public and abstract. */
 interface AlertDAO {
-   public abstract UUID addAlert(Date time);
-   public abstract Date getAlert(UUID id); 
+    UUID addAlert(Date time);
+
+    Date getAlert(UUID id);
 }
 
 class AlertService {
     private AlertDAO storage;
-  
-    public AlertService(AlertDAO alert){
-       this.storage = alert;
+
+    public AlertService(AlertDAO alert) {
+        this.storage = alert;
     }
-		
+
     public UUID raiseAlert() {
         return this.storage.addAlert(new Date());
     }
-	
+
     public Date getAlertTime(UUID id) {
         return this.storage.getAlert(id);
-    }	
+    }
 }
 
 class MapAlertDAO implements AlertDAO {
     private final Map<UUID, Date> alerts = new HashMap<UUID, Date>();
-	
+
     public UUID addAlert(Date time) {
-    	UUID id = UUID.randomUUID();
+        UUID id = UUID.randomUUID();
         this.alerts.put(id, time);
         return id;
     }
-	
+
     public Date getAlert(UUID id) {
         return this.alerts.get(id);
-    }	
+    }
 }

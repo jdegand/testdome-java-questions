5fe978538e1c4d2db98b8f20d3cee0e07dbe08ef
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-12-11 19:21:02 +0000

    fix: update notes in BoatMovements

--- Java/BoatMovements.java
+++ Java/BoatMovements.java
@@ -1,28 +1,34 @@
-/* Note: This question has broken validation. */
+/* Note: Testdome validation seems to be broken as this returns the intended answer(true, false, false)  */
 public class BoatMovements {
     /**
      * @return boolean The destination is reachable or not
      */
     public static boolean canTravelTo(boolean[][] gameMatrix, int fromRow, int fromColumn, int toRow, int toColumn) {
-        boolean validMove = true;
-
-        if (toRow < 0 || toColumn < 0 || toRow >= gameMatrix.length || toColumn >= gameMatrix[0].length ||
-                fromRow < 0 || fromColumn < 0 || fromRow >= gameMatrix.length || fromColumn >= gameMatrix[0].length ||
-                !gameMatrix[toRow][toColumn]) {
-            validMove = false;
-        } else if (fromRow == toRow) {
-            if (toColumn < fromColumn - 1 || toColumn > fromColumn + 1) {
-                validMove = false;
-            } else if (toColumn == fromColumn + 1 && !gameMatrix[fromRow][fromColumn + 1]) {
-                validMove = false;
-            }
-        } else if (toRow == fromRow - 1 || toRow == fromRow + 1) {
-            if (toColumn != fromColumn) {
-                validMove = false;
-            }
+        // Validate if destination is out of bounds or starting point is out of bounds
+        if (!isValidPosition(gameMatrix, toRow, toColumn) || !isValidPosition(gameMatrix, fromRow, fromColumn)) {
+            return false;
         }
-        
-        return validMove;
+
+        // Validate if destination is a valid cell
+        if (!gameMatrix[toRow][toColumn]) {
+            return false;
+        }
+
+        // Check movement within the same row
+        if (fromRow == toRow) {
+            return Math.abs(toColumn - fromColumn) == 1;
+        }
+
+        // Check movement within the same column
+        if (fromColumn == toColumn) {
+            return Math.abs(toRow - fromRow) == 1;
+        }
+
+        return false;
+    }
+
+    private static boolean isValidPosition(boolean[][] matrix, int row, int column) {
+        return row >= 0 && column >= 0 && row < matrix.length && column < matrix[0].length;
     }
 
     public static void main(String[] args) {
@@ -39,5 +45,4 @@ public class BoatMovements {
         System.out.println(canTravelTo(gameMatrix, 3, 2, 3, 4) ? "true" : "false"); // false, Can't travel through land
         System.out.println(canTravelTo(gameMatrix, 3, 2, 5, 2) ? "true" : "false"); // false, Out of bounds
     }
-
 }

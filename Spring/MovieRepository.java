9acb3cad9b511ac2f1d3518e2a6cc4c1bbdfeac5
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-12-11 19:54:11 +0000

    fix: remove null in MovieRepository

--- Spring/MovieRepository.java
+++ Spring/MovieRepository.java
@@ -1,17 +1,17 @@
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.*;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import javax.annotation.PostConstruct;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.*;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import java.util.List;
+import java.util.ArrayList;
 
 class Movie {
     public String name;
     public int year;
     public int rating;
-    
+
     public Movie(String name, int year, int rating) {
         this.name = name;
         this.year = year;
@@ -27,16 +27,17 @@ public class MovieRepository {
 
     @PostConstruct
     public void createTable() {
-        template.execute("CREATE TABLE movies (id bigint auto_increment primary key, name VARCHAR(50), year int, rating int)");
+        template.execute(
+                "CREATE TABLE movies (id bigint auto_increment primary key, name VARCHAR(50), year int, rating int)");
     }
 
     public void createMovie(String name, int year, int rating) {
-        template.update("INSERT INTO movies (id, name, year, rating) VALUES (?,?,?,?)", 
-                        null, name, year, rating);
+        template.update("INSERT INTO movies (name, year, rating) VALUES (?,?,?)", name, year, rating);
     }
-    
+
     public List<Movie> findMoviesByName(String nameStartsWith) {
-       return template.query("SELECT * FROM movies WHERE name LIKE '" + nameStartsWith + "%'", 
-              (rs, rowNum) -> new Movie(rs.getString("name"), rs.getInt("year"), rs.getInt("rating")));
+        String sql = "SELECT * FROM movies WHERE name LIKE ?";
+        return template.query(sql, new Object[] { nameStartsWith + "%" }, (rs, rowNum) -> new Movie(
+                rs.getString("name"), rs.getInt("year"), rs.getInt("rating")));
     }
 }

163e17793e3f07ecab8346afa045016fbaaec8e4
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-12-11 19:25:30 +0000

    refactor: use intended binary search

--- Java/SortedSearch.java
+++ Java/SortedSearch.java
@@ -1,15 +1,18 @@
+import java.util.Arrays;
+
 public class SortedSearch {
-    public static int countNumbers(int[] sortedArray, int lessThan) {
-        int count = 0;
-        for(int element : sortedArray){
-          if(element < lessThan){
-            count++;
-          }
-        }
-        return count;
+  public static int countNumbers(int[] sortedArray, int lessThan) {
+    int index = Arrays.binarySearch(sortedArray, lessThan);
+
+    // If the element is not found, binarySearch returns (-(insertion point) - 1)
+    if (index < 0) {
+      index = -index - 1;
     }
-    
-    public static void main(String[] args) {
-        System.out.println(SortedSearch.countNumbers(new int[] { 1, 3, 5, 7 }, 4));
-    }
+
+    return index;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(SortedSearch.countNumbers(new int[] { 1, 3, 5, 7 }, 4)); // Should print 2
+  }
 }

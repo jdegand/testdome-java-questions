e15abdbd9ffa56c26074f68b0a9c0ff8a82e8f2b
J. Degand <70610011+jdegand@users.noreply.github.com>
2024-12-11 19:12:39 +0000

    fix: try with resources to close stream

--- Java/DecoratorStream.java
+++ Java/DecoratorStream.java
@@ -6,8 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 
-public class DecoratorStream extends OutputStream
-{
+public class DecoratorStream extends OutputStream {
    private OutputStream stream;
    private String prefix;
 
@@ -19,14 +18,11 @@ public class DecoratorStream extends OutputStream
 
    @Override
    public void write(int b) throws IOException {
-      byte[] result = new byte[4];
-
-      result[0] = (byte) (b >> 24);
-      result[1] = (byte) (b >> 16);
-      result[2] = (byte) (b >> 8);
-      result[3] = (byte) (b);
-
-      write(result, 0, 4);
+      if (prefix != null) {
+         stream.write(prefix.getBytes(StandardCharsets.UTF_8));
+         prefix = null;
+      }
+      stream.write(b);
    }
 
    @Override
@@ -43,14 +39,20 @@ public class DecoratorStream extends OutputStream
       write(b, 0, b.length);
    }
 
+   @Override
+   public void close() throws IOException {
+      stream.close();
+   }
+
    public static void main(String[] args) throws IOException {
-      byte[] message = new byte[]{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21};
-      try(ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-         DecoratorStream decoratorStream = new DecoratorStream(baos, "First line: ");
+      byte[] message = new byte[] { 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21 };
+      try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            DecoratorStream decoratorStream = new DecoratorStream(baos, "First line: ")) {
          decoratorStream.write(message);
 
-         try(BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(baos.toByteArray()), "UTF-8"))) {
-            System.out.println(reader.readLine());  //should print "First line: Hello, world!"
+         try (BufferedReader reader = new BufferedReader(
+               new InputStreamReader(new ByteArrayInputStream(baos.toByteArray()), StandardCharsets.UTF_8))) {
+            System.out.println(reader.readLine()); // should print "First line: Hello, world!"
          }
       }
    }

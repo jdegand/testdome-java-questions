55a84adc542ea578075a64466b5ba011b6a8e177
jdegand <70610011+jdegand@users.noreply.github.com>
2024-12-21 15:29:31 -0500

    fix: import in PetDAO and remove unnecessary variable

--- Hibernate/PetDAO.java
+++ Hibernate/PetDAO.java
@@ -5,24 +5,23 @@
 
 import java.util.*;
 import java.util.logging.*;
-import javax.persistence.*;
+import jakarta.persistence.*;
 import org.hibernate.*;
 import org.hibernate.cfg.*;
 import org.hibernate.query.Query;
 
 public class PetDAO {
-    
+
     public List<Pet> findNamedPetsBySpecies(Session session, String species) {
-      List<Pet> pets = session.createNativeQuery(
-        "SELECT * " +
-        "FROM Pet " +
-        "WHERE species like :species AND name is NOT NULL ")
-        .addEntity( Pet.class )
-        .setParameter("species", species)
-        .list();
-      return pets;
+        return session.createNativeQuery(
+                "SELECT * " +
+                        "FROM Pet " +
+                        "WHERE species like :species AND name is NOT NULL")
+                .addEntity(Pet.class)
+                .setParameter("species", species)
+                .list();
     }
-    
+
     @Entity(name = "Pet")
     @Table
     public static class Pet {
@@ -32,16 +31,17 @@ public class PetDAO {
         public String name;
         @Column
         public String species;
-        
-        public Pet() {}
-        
+
+        public Pet() {
+        }
+
         public Pet(Integer id, String name, String species) {
             this.id = id;
             this.name = name;
             this.species = species;
         }
     }
-    
+
     public static void main(String[] args) {
         LogManager logManager = LogManager.getLogManager();
         Logger logger = logManager.getLogger("");
@@ -53,15 +53,15 @@ public class PetDAO {
         prop.setProperty("hibernate.hbm2ddl.auto", "create");
 
         SessionFactory sessionFactory = new Configuration().addProperties(prop)
-            .addAnnotatedClass(Pet.class).buildSessionFactory();
-        
+                .addAnnotatedClass(Pet.class).buildSessionFactory();
+
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        
+
         Pet dog = new Pet(0, "Lady", "Dog");
         Pet cat = new Pet(1, "Max", "Cat");
         Pet camel = new Pet(2, null, "Camel");
-        
+
         session.save(dog);
         session.save(cat);
         session.save(camel);
@@ -69,7 +69,7 @@ public class PetDAO {
 
         PetDAO petDao = new PetDAO();
         List<Pet> pets = petDao.findNamedPetsBySpecies(session, "Cat");
-        for(Pet p : pets) {
+        for (Pet p : pets) {
             System.out.println(p.id);
             System.out.println(p.name);
             System.out.println(p.species);

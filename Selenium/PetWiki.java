3aa2427ffd9dd0ed5b0884f9262de2cfab5a28c2
jdegand <70610011+jdegand@users.noreply.github.com>
2024-12-21 14:48:19 -0500

    refactor: PetWiki to use loadBtn variable

--- Selenium/PetWiki.java
+++ Selenium/PetWiki.java
@@ -8,28 +8,24 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import java.time.Duration;
 
 public class PetWiki {
-    public static String getLoadedData(WebDriver driver) {
+   public static String getLoadedData(WebDriver driver) {
 
-      // Don't need to grab the page -> already loaded by testdome
-      // driver.get("https://www.testdome.com/resources/media?name=cb8dcf4b-73ba-4ca3-aff4-7ba19d9a6a08/example_case.txt");
-        
+      // Don't need to grab the page with driver get -> already loaded by testdome
+
       WebElement loadBtn = driver.findElement(By.id("load-button"));
       Wait<WebDriver> wait = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(1))
-                .pollingEvery(Duration.ofMillis(250))
-                .ignoring(ElementNotInteractableException.class);
+            .withTimeout(Duration.ofSeconds(1))
+            .pollingEvery(Duration.ofMillis(250))
+            .ignoring(ElementNotInteractableException.class);
 
-      driver.findElement(By.id("load-button")).click();
-      
-        try {
-          
-           wait.until(ExpectedConditions.elementToBeClickable(By.id("load-button")));
-           WebElement content = driver.findElement(By.id("content"));
-           return content.getText();
-          
-        } catch (TimeoutException ex) {
-           return null;
-        }
-      
-    }
+      loadBtn.click();
+
+      try {
+         wait.until(ExpectedConditions.elementToBeClickable(loadBtn));
+         return driver.findElement(By.id("content")).getText();
+      } catch (TimeoutException ex) {
+         return null;
+      }
+
+   }
 }

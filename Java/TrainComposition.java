e8719593762f1f4cda924e64e269828db382caab
jdegand <70610011+jdegand@users.noreply.github.com>
2024-12-22 15:29:38 -0500

    refactor: train composition remove intermediate variables

--- Java/TrainComposition.java
+++ Java/TrainComposition.java
@@ -4,12 +4,11 @@ public class TrainComposition {
     // implementation same as my csharp solution
 
     private LinkedList<Integer> train;
-  
-    public TrainComposition() 
-    {
-      train = new LinkedList<>();
+
+    public TrainComposition() {
+        train = new LinkedList<>();
     }
-  
+
     public void attachWagonFromLeft(int wagonId) {
         train.addFirst(wagonId);
     }
@@ -19,20 +18,18 @@ public class TrainComposition {
     }
 
     public int detachWagonFromLeft() {
-         int node = train.removeFirst();
-         return node;
+        return train.removeFirst();
     }
 
     public int detachWagonFromRight() {
-        int node = train.removeLast();
-        return node;
+        return train.removeLast();
     }
 
     public static void main(String[] args) {
         TrainComposition train = new TrainComposition();
         train.attachWagonFromLeft(7);
         train.attachWagonFromLeft(13);
-        System.out.println(train.detachWagonFromRight()); // 7 
+        System.out.println(train.detachWagonFromRight()); // 7
         System.out.println(train.detachWagonFromLeft()); // 13
     }
 }
